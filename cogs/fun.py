918b9d676c5df113468990b7de012fe591752a9d
Kat <61893437+TransKat@users.noreply.github.com>
2021-09-04 12:15:36 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,7 @@
 import discord
 import random
 import time
+import json
 from discord.ext import commands
 
 
@@ -27,6 +28,7 @@ class Fun(commands.Cog):
     async def say(self, ctx, *, arg):
         await ctx.message.delete()
         await ctx.send(arg)
+        print(f"{ctx.author} used say to send {arg} in {ctx.channel.id}")
 
     @commands.command(brief="Generates a random number from 0 to a number of your choice.")
     async def gennumber(self, ctx, arg):
@@ -71,48 +73,41 @@ class Fun(commands.Cog):
     @commands.command(brief="Have fun dying.")
     async def die(self, ctx):
         value = random.randint(0, 14)
+        await ctx.send(json.load(open('response-die.json')).get(f'{value}'))
+        await ctx.send(json.load(open('die-rhetorical.json')).get(f'{value}'))
+
+    @commands.command(brief="murder each other!")
+    async def kill(self, ctx, member:discord.User):
+        value = random.randint(0, 14)
         if value == 0:
-            await ctx.send(f"<@{ctx.author.id}> tried to swim in lava")
-            await ctx.send("lmao this isn't minecraft dumbass")
+            await ctx.send(f"<@{ctx.author.id}> pushed <@{member.id}> into lava")
         elif value == 1:
-            await ctx.send(f"<@{ctx.author.id}> saw someone tweet \"ratio\" and died of cringe")
-            await ctx.send("lmao fucking twitter addict")
+            await ctx.send(f"<@{ctx.author.id}> tweeted cringe but <@{member.id}> saw it and died of cringe")
         elif value == 2:
-            await ctx.send(f"<@{ctx.author.id}> posted cringe and was cancelled to death")
-            await ctx.send("ratio")
+            await ctx.send(f"<@{ctx.author.id}> stabbed @<{member.id}")
         elif value == 3:
-            await ctx.send(f"<@{ctx.author.id}>\'s computer overheated and exploded killing them")
-            await ctx.send("lmao dumbass was probably trying to play real life simulator")
+            await ctx.send(f"<@{ctx.author.id}> \"accidentally\" shot <@{member.id}>")
         elif value == 4:
-            await ctx.send(f"<@{ctx.author.id}> choked on a fork")
-            await ctx.send("lmao dumbass how do you choke on a fork")
+            await ctx.send(f"<@{ctx.author.id}> poisoned <@{member.id}>\'s food")
         elif value == 5:
-            await ctx.send(f"<@{ctx.author.id}> forgot to breathe")
-            await ctx.send("LMAO DUMBASS")
+            await ctx.send(f"<@{ctx.author.id}> removed <@{member.id}>\'s lungs")
         elif value == 6:
-            await ctx.send(f"<@{ctx.author.id}> thought they could breathe water")
-            await ctx.send("they are sleeping with the fish")
+            await ctx.send(f"<@{ctx.author.id}> sent <@{member.id}> a bomb in the mail")
         elif value == 7:
-            await ctx.send(f"<@{ctx.author.id}> fell down a hole while drunk and was never seen again")
-            await ctx.send("sounds like my dad")
+            await ctx.send(f"<@{ctx.author.id}> got <@{member.id}> so drunk they fell off a building and didn't bounce")
         elif value == 8:
-            await ctx.send(f"<@{ctx.author.id}> was hit by a falling hailstone and died")
-            await ctx.send("you absolute dumb fuck")
+            await ctx.send(f"<@{ctx.author.id}> had too much drip and <@{member.id}> died of jealousy")
         elif value == 9:
-            await ctx.send(f"<@{ctx.author.id}> opened the fridge but could not make a choice and starved")
-            await ctx.send("professional dumbass here")
+            await ctx.send(f"<@{ctx.author.id}> stole <@{member.id}>\'s fridge and they starved")
         elif value == 10:
-            await ctx.send(f"<@{ctx.author.id}> disrespected Sylveon and was smitten")
+            await ctx.send(f"<@{member.id}> disrespected Sylveon and was murdered by <@{ctx.author.id}>")
             await ctx.send("This... does put a smile on my face.")
         elif value == 11:
-            await ctx.send(f"<@{ctx.author.id}> was on the bad side of the 50%")
-            await ctx.send("lmao better luck next time loser")
+            await ctx.send(f"<@{ctx.author.id}> deleted <@{member.id}> with the power of Sylveon")
         elif value == 12:
-            await ctx.send(f"<@{ctx.author.id}> ordered a sylveon plush online but it was swapped with anthrax")
-            await ctx.send("that\'s just unlucky...")
+            await ctx.send(f"<@{member.id}> ordered a sylveon plush online but <@{ctx.author.id}> swapped it with anthrax")
         else:
-            await ctx.send(f"<@{ctx.author.id}> died in some horrific way unexplainable on Discord")
-            await ctx.send("no comment loser")
+            await ctx.send(f"<@{ctx.author.id}> murdered <@{member.id}> in some horrific way unexplainable on Discord")
 
 
 def setup(client):
